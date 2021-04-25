7b85d02fbcdb3d8bd85d91c04fb8f9786df92374
DESKTOP-85HKM11\Chiu Park Chuan <parkchuan94@hotmail.com>
2021-04-25 10:56:22 +0800

    sentiment analytic kubernetes files

---
+++ init_stack.ps1
@@ -0,0 +1,28 @@
+﻿#Prepare configmap
+$configMapName = "sentiment-analytic-py-config"
+$res = kubectl get configmap $configMapName
+Write-Output $res[1].GetType().FullName
+if($res[1].Contains($configMapName)){
+    kubectl delete configmap $configMapName
+}
+
+$configMapName = "sentiment-analytic-go-config"
+$res = kubectl get configmap $configMapName
+Write-Output $res[1].GetType().FullName
+if($res[1].Contains($configMapName)){
+    kubectl delete configmap $configMapName
+}
+
+$configMapName = "sentiment-collector-config"
+$res = kubectl get configmap $configMapName
+Write-Output $res[1].GetType().FullName
+if($res[1].Contains($configMapName)){
+    kubectl delete configmap $configMapName
+}
+kubectl create configmap sentiment-analytic-py-config --from-file="D:\SharkDetector\src\SentimentAnalyticPy\.env"
+kubectl create configmap sentiment-analytic-go-config --from-file="D:\SharkDetector\src\SentimentAnalytic\.env"
+kubectl create configmap sentiment-collector-config --from-file="D:\SharkDetector\src\yf-conversations-fiberv2\.env"
+
+
+
+
